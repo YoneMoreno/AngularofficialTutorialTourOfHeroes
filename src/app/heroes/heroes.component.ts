8975ed0ca9baad7ddf2477f0cbd8ea327bbfc519
YonePC <YonePC@YONE>
2018-01-06 15:48:48 +0000

    removed unused code from heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,7 @@ import {MyHeroService} from '../my-hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   constructor(private myHeroService: MyHeroService) {
   }
