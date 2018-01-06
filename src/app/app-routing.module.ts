ede19ee2e5ab76563470697dbb4e340cadf9fa54
YonePC <YonePC@YONE>
2018-01-06 14:46:43 +0000

    we deleted declarations array because of it usual to not declare components into a routing module. Also removed CommonModule because of it is not needed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
