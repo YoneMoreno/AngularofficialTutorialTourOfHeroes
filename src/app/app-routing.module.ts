c66afc2460398456c1d7fa70863354824de6db91
YonePC <YonePC@YONE>
2018-01-06 15:03:03 +0000

    routes well defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 
-export class AppRoutingModule {
-  const;
-  routes: Routes = [
-    {path: 'heroes', component: HeroesComponent}
-  ];
-}
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)]
 })
+export class AppRoutingModule {
+
+}
