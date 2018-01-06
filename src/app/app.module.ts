00c9494d6f51b00f69c0a2bf56e0956bb2d86581
YonePC <YonePC@YONE>
2018-01-06 15:12:17 +0000

    created dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MyHeroService} from "./my-hero.service";
 import {MessagesComponent} from './messages/messages.component';
 import {MessageService} from './message.service';
 import {AppRoutingModule} from "./app-routing.module";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import {AppRoutingModule} from "./app-routing.module";
     AppComponent,
     HeroesComponent,
     HeroDetailsComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
