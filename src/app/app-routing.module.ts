8e2995a5f79cab19c1106367876f22b92c3d6bc6
YonePC <YonePC@YONE>
2018-01-06 15:37:42 +0000

    added detail route with id query string

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeroDetailsComponent} from "./hero-details/hero-details.component";
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
+  {path: 'detail/:d', component: HeroDetailsComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
