eafefafed534d0de8b92aac58d3ae48562cd78dd
YonePC <YonePC@YONE>
2018-01-06 10:50:15 +0000

    heroe is being displayed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  hero = 'Windstorm';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
