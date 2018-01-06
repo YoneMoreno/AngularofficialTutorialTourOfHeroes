9d64ec2c23584062e37e7c412b13da9e0e8093e1
YonePC <YonePC@YONE>
2018-01-06 13:38:44 +0000

    implementing hero retrieval service

--- src/app/my-hero.service.ts
+++ src/app/my-hero.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
-
+import {Injectable} from '@angular/core';
+import {HEROES} from "./mock-heroes";
+import {Hero} from "./Hero";
 
 @Injectable()
 export class MyHeroService {
 
-  constructor() { }
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+  constructor() {
+  }
 
 }
