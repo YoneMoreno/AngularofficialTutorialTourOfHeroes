400196ebe4bed44304139b75f195aa8a42293302
YonePC <YonePC@YONE>
2018-01-06 13:59:29 +0000

    add asynchronous behaviour to get heroes service

--- src/app/my-hero.service.ts
+++ src/app/my-hero.service.ts
@@ -1,12 +1,14 @@
 import {Injectable} from '@angular/core';
 import {HEROES} from './mock-heroes';
 import {Hero} from './Hero';
+import {Observable} from "rxjs/Observable";
+import {of} from "rxjs/observable/of";
 
 @Injectable()
 export class MyHeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
   constructor() {
