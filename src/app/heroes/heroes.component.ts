b3d0062d1d54869f61b91a15ba91c49b19fa8b91
YonePC <YonePC@YONE>
2018-01-06 11:44:59 +0000

    mocked heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../Hero';
+import {HEROES} from "../mock-heroes";
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +9,7 @@ import {Hero} from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
