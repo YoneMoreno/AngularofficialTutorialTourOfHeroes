5ae6bc6928bba2caefe2b51b51de843b8d212eba
YonePC <YonePC@YONE>
2018-01-06 14:58:21 +0000

    push up class to be able to initialize the Router with the routes variable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
-import {HeroesComponent} from "./heroes/heroes.component";
+import {RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
 
-@NgModule({
-  exports: [RouterModule]
-})
 export class AppRoutingModule {
-  const
+  const;
   routes: Routes = [
     {path: 'heroes', component: HeroesComponent}
   ];
 }
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
+})
