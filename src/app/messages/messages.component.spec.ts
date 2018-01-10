76dcfd84f3615eea2df9355fd2930687d760d6d8
YonePC <YonePC@YONE>
2018-01-10 06:13:45 +0000

    green: clear messages

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -4,12 +4,26 @@ describe('MessageService', () => {
 
   describe('addMessage', () => {
     it('should add the message to the array', () => {
-      let messages: string[] = [];
+      const messages: string[] = [];
       const messageService = new MessageService(messages);
 
       messageService.add('Hero message');
 
       expect(messageService.getMessages().length).toBe(1);
+    });
+
+    describe('clearMessage', () => {
+      it('should clear all the messages', () => {
+
+        const messages: string[] = ['anyMessage'];
+        const messageService = new MessageService(messages);
+
+        expect(messageService.getMessages().length).toBe(1);
+        messageService.clear();
+
+        expect(messageService.getMessages().length).toBe(0);
+
+      });
     });
   });
 });
